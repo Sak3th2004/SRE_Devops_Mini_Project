88c282c3155b791b8988afc52a5319b524524e23
Sak3th2004 <gunnamsaketh00@gmail.com>
2026-09-07 16:38:59 +0530

    bump fallback version to 1.0.1

--- dashboard/config.py
+++ dashboard/config.py
@@ -3,5 +3,5 @@ import os
 
 class Config:
     APP_ENV = os.environ.get("APP_ENV", "development")
-    APP_VERSION = os.environ.get("APP_VERSION", "1.0.0")
+    APP_VERSION = os.environ.get("APP_VERSION", "1.0.1")
     PORT = int(os.environ.get("PORT", "5000"))
